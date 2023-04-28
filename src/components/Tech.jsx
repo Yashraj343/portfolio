9214e8b846fa48f9deb2ee67aec13da9be502fbb
Yashraj <yashrajpatil056@gmail.com>
2023-04-29 01:06:44 +0530

    added unique keys in tech.js file

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -28,29 +28,58 @@
 // };
 
 // export default SectionWrapper(Tech, "");
+// import {BallCanvas} from "./canvas";
+// import {SectionWrapper} from "../hoc";
+// import { technologies } from "../constants";
+
+//   const Title=()=>{
+//     return(
+//       <h1 className="mb-20 flex justify-center font-extrabold text-[44px] text-gray-400">Some of the technologies I'm familiar with...</h1>
+//     )
+//   }
+  
+// const Tech = () => {
+//   return (
+//     <div >
+//       <Title />
+//     <div className="flex flex-row flex-wrap justify-center gap-10 ">
+//       {technologies.map((technology)=>(
+//         <div className=" w-28 h-28 key={technology.name}">
+//             <BallCanvas icon={technology.icon}></BallCanvas>
+//         </div>
+//       ))}
+//     </div>
+//     </div>
+//   )
+// }
+
+// export default SectionWrapper(Tech,"");
+
 import {BallCanvas} from "./canvas";
 import {SectionWrapper} from "../hoc";
 import { technologies } from "../constants";
 
-  const Title=()=>{
-    return(
-      <h1 className="mb-20 flex justify-center font-extrabold text-[44px] text-gray-400">Some of the technologies I'm familiar with...</h1>
-    )
-  }
-  
+const Title = () => {
+  return (
+    <h1 className="mb-20 flex justify-center font-extrabold text-[44px] text-gray-400">
+      Some of the technologies I'm familiar with...
+    </h1>
+  );
+};
+
 const Tech = () => {
   return (
-    <div >
+    <div>
       <Title />
-    <div className="flex flex-row flex-wrap justify-center gap-10 ">
-      {technologies.map((technology)=>(
-        <div className=" w-28 h-28 key={technology.name}">
-            <BallCanvas icon={technology.icon}></BallCanvas>
-        </div>
-      ))}
+      <div className="flex flex-row flex-wrap justify-center gap-10 ">
+        {technologies.map((technology) => (
+          <div className="w-28 h-28" key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default SectionWrapper(Tech,"");
+export default SectionWrapper(Tech, "");
